ff2a40ddbcd433d7e0f6bb43a778438f06ca00c1
40food <40food@naver.com>
2026-02-11 12:37:32 +0900

    chore: 프로젝트 구조 수정

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@hotspot/shared/ui';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <Button appName={''}>test</Button>
+    </div>
+  );
+};
+
+export default page;
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@hotspot/shared/ui';
+
+const page = () => {
+  return (
+    <div>
+      <Button appName={''}>test</Button>
+    </div>
+  );
+};
+
+export default page;
