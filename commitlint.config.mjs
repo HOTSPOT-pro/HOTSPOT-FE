f1b38ea8ae74c47c24493132322cf5246133384a
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-02-12 18:25:15 +0900

    feat: husky, lint 추가

---
+++ commitlint.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'ci', 'revert', 'del', 'add'],
+    ],
+  },
+};
