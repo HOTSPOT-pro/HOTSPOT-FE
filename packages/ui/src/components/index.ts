1288602694bf485a3719627e3607ad486ff0b5e7
JIYEON <144954836+Chaejy@users.noreply.github.com>
2026-02-22 20:44:19 +0900

    [HOTSPOT-86] 카드 컴포넌트 구현 (#34)
    * [HOTSPOT-86] feat: 카드 컴포넌트 제작
    
    * [HOTSPOT-86] feat: index에 카드 추가
    
    * [HOTSPOT-86] docs: style guide 수정
    
    * [HOTSPOT-86] fix: card 텍스트 렌더링 수정
    
    * [HOTSPOT-86] feat: 카드 요소 분할
    
    * [HOTSPOT-86] feat: card component 요소에 flex 추가
    
    * fix: index.ts 수정

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,12 @@
 export { Button } from './button/Button';
-// export { Card } from './card/Card';
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './card/Card';
 export { DonutChartContainer } from './chart/donut/DonutChartContainer';
 export { LineChart } from './chart/line/LineChart';
 export { ProgressBar } from './chart/progressbar/ProgressBar';
