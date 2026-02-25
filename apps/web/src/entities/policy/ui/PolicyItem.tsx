3f28a6a276d0681bc6365fff0de3d74b58931e7f
40food <40food@naver.com>
2026-02-25 20:15:27 +0900

    [HOTSPOT-76] fix: 코드 리뷰 반영

--- apps/web/src/entities/policy/ui/PolicyItem.tsx
+++ apps/web/src/entities/policy/ui/PolicyItem.tsx
@@ -1,12 +1,13 @@
+import type { ReactNode } from 'react';
 import type { Policy } from '../model/type';
 
 interface PolicyItemProps {
   item: Policy;
-  icon: React.ReactNode;
+  icon: ReactNode;
 }
 
 export const PolicyItem = ({ item, icon }: PolicyItemProps) => (
-  <div className="bg-white rounded-xl px-3 py-2.5 flex-row flex items-center gap-2.5" key={item.id}>
+  <div className="bg-white rounded-xl px-3 py-2.5 flex-row flex items-center gap-2.5">
     {icon}
     <div className="flex flex-col">
       <div className="font-normal text-sm">{item.name}</div>
