43c7e1a826bc5c1d118dbe085382494c09ecbe31
40food <40food@naver.com>
2026-02-26 13:30:39 +0900

    [HOTSPOT-75] feat: 에러 모달 추가

--- apps/web/src/features/onboarding/model/useOnboarding.ts
+++ apps/web/src/features/onboarding/model/useOnboarding.ts
@@ -1,13 +1,25 @@
 'use client';
 
+import { useModal } from '@hotspot/ui';
 import { toPureDigits } from '@shared/lib';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { useCallback } from 'react';
 import { postOnboarding } from '../api/postOnboarding';
+import { ERROR_MESSAGE } from '../constants/messages';
 import type { OnboardingInfo } from './types';
 
 export const useOnboarding = () => {
   const router = useRouter();
+  const { open } = useModal();
+  const handleOpenModal = useCallback(() => {
+    open('errorModal', {
+      props: {
+        content: ERROR_MESSAGE.ONBOARDING_ERROR.message,
+        title: ERROR_MESSAGE.ONBOARDING_ERROR.title,
+      },
+    });
+  }, [open]);
 
   const { mutate, isPending, error } = useMutation({
     mutationFn: (data: OnboardingInfo) =>
@@ -16,8 +28,8 @@ export const useOnboarding = () => {
         phoneNumber: toPureDigits(data.tel),
       }),
 
-    onError: (err) => {
-      console.error('온보딩 처리 중 에러:', err);
+    onError: () => {
+      handleOpenModal();
     },
     onSuccess: () => {
       router.replace('/');
