8afc56f6ce31be4a70ecce862ba5dbae031b15f5
JIYEON <144954836+Chaejy@users.noreply.github.com>
2026-03-03 16:52:46 +0900

    [HOTSPOT-77] feat: 개인정보 slice에 저장 및 마이페이지 구현 (#71)

---
+++ apps/web/src/app/(shell)/(sub)/family/page.tsx
@@ -0,0 +1,7 @@
+import { FamilyPage } from '@/pages-layer/family/ui/FamilyPage';
+
+const Page = () => {
+  return <FamilyPage />;
+};
+
+export default Page;
---
+++ apps/web/src/pages-layer/family/ui/FamilyPage.tsx
@@ -0,0 +1,3 @@
+export const FamilyPage = () => {
+  return <div></div>;
+};
