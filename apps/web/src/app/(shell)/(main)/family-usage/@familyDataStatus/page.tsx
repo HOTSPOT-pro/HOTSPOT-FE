7bcdbe73a8e30720840a6d5697168897e3bf5ae8
JIYEON <144954836+Chaejy@users.noreply.github.com>
2026-03-03 14:38:30 +0900

    [HOTSPOT-72] feat: 가족 데이터 현황 페이지 제작 (#64)

---
+++ apps/web/src/app/(shell)/(main)/family-usage/@familyDataStatus/page.tsx
@@ -0,0 +1,7 @@
+import { FamilyDataStatusPage } from '@/pages-layer/family-data-status/ui/FamilyDataStatusPage';
+
+const page = () => {
+  return <FamilyDataStatusPage />;
+};
+
+export default page;
--- packages/ui/src/components/chart/line/LineChartLegend.tsx
+++ packages/ui/src/components/chart/line/LineChartLegend.tsx
@@ -1,4 +1,6 @@
 /** biome-ignore-all lint/suspicious/noExplicitAny: <LegendProps에서 payload 못 불러옴> */
+import { LegendItem } from '../legend/LegendItem';
+
 export const LineChartLegend = (props: any) => {
   const { payload } = props;
   if (!payload) return null;
@@ -6,15 +8,12 @@ export const LineChartLegend = (props: any) => {
   return (
     <div className="flex justify-center gap-5 mt-7.5">
       {payload.map((entry: any, index: number) => (
-        <div className="flex items-center gap-2" key={`legend-${index}`}>
-          <div
-            className="w-4 h-1 rounded-full"
-            style={{
-              backgroundColor: entry.color,
-            }}
-          />
-          <span className="text-sm font-medium text-gray-600">{entry.value}</span>
-        </div>
+        <LegendItem
+          className="justify-start"
+          color={entry.color}
+          key={`legend-${index}`}
+          name={entry.value}
+        />
       ))}
     </div>
   );
