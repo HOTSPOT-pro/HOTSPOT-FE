190e17c978d04540a154a5cf3536cf093035ccc4
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-03-05 16:33:52 +0900

    [HOTSPOT-82] feat: 카테고리 선택 컴포넌트 구현

--- apps/admin/src/shared/ui/index.ts
+++ apps/admin/src/shared/ui/index.ts
@@ -1,6 +1,7 @@
-export * from './header/Header';
+export { CategorySelect } from './category-select/CategorySelect';
+export { Header } from './header/Header';
 export { Pagination } from './pagination/Pagination';
-export * from './search-bar/SearchBar';
-export * from './side-bar/SideBar';
+export { SearchBar } from './search-bar/SearchBar';
+export { SideBar } from './side-bar/SideBar';
 export type { Column } from './table/Table';
 export { Table } from './table/Table';
