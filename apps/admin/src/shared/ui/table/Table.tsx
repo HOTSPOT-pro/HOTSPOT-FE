e5efc6a711d920348f5030770596c0adeb9e391a
박승연 <70251709+40food@users.noreply.github.com>
2026-03-05 09:36:20 +0900

    [HOTSPOT-79] 테이블, 페이지네이션 컴포넌트 제작 (#84)
    * [HOTSPOT-79] feat: 테이블 및 페이지네이션 컴포넌트 추가
    
    * [HOTSPOT-79] fix: run시 호스트명 붙도록 수정
    
    * [HOTSPOT-79] fix: 없는 컴포넌트 주석 처리
    
    * [HOTSPOT-79] fix: 코드 리뷰 반영
    
    * [HOTSPOT-79] fix: pagination 이동 오류 수정
    
    * [HOTSPOT-79] fix: 테스트용 페이지 삭제
    
    * [HOTSPOT-79] fix: table 및 pagenation 수정
    
    * [HOTSPOT-79] fix: pagination 현재 페이지 클릭시 onMove 미호출로 변경

---
+++ apps/admin/src/shared/ui/table/Table.tsx
@@ -0,0 +1,88 @@
+import type { ReactNode } from 'react';
+
+export interface Column<T> {
+  header: string; // 칼럼 이름
+  accessor: keyof T | 'actions'; // 데이터의 키 값 또는 버튼 등 커스텀 액션용
+  render?: (value: any, row: T) => ReactNode; // 커스텀 렌더링 함수
+}
+
+export interface DynamicTableProps<T> {
+  columns: Column<T>[];
+  data: T[];
+  isLoading?: boolean;
+}
+
+export const Table = <T extends { id: string | number }>({
+  columns,
+  data,
+  isLoading,
+}: DynamicTableProps<T>) => {
+  const renderBodyContent = () => {
+    // 로딩 중일 때
+    if (isLoading) {
+      return [...Array(10)].map((_, rowIndex) => (
+        <tr className="border-b border-gray-50" key={`skeleton-row-${rowIndex}`}>
+          {columns.map((_, colIndex) => (
+            <td className="px-6 py-5" key={`skeleton-col-${colIndex}`}>
+              <div className="h-7 bg-gray-100 rounded-md animate-pulse w-full" />
+            </td>
+          ))}
+        </tr>
+      ));
+    }
+
+    // 데이터가 없을 때
+    if (data.length === 0) {
+      return (
+        <tr>
+          <td className="px-6 py-20 text-center text-gray-400 font-medium" colSpan={columns.length}>
+            데이터가 존재하지 않습니다.
+          </td>
+        </tr>
+      );
+    }
+
+    // 정상 데이터가 있을 때
+    return data.map((row) => (
+      <tr className="hover:bg-gray-50/50 transition-colors group" key={row.id}>
+        {columns.map((col, index) => {
+          const cellValue =
+            col.accessor !== 'actions' ? (row[col.accessor as keyof T] as React.ReactNode) : null;
+
+          return (
+            <td
+              className="px-6 py-4 whitespace-nowrap text-black text-[14px] font-normal"
+              key={`cell-${row.id}-${index}`}
+            >
+              {col.render ? col.render(cellValue, row) : cellValue}
+            </td>
+          );
+        })}
+      </tr>
+    ));
+  };
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full text-sm text-left border-collapse">
+        {/* Header */}
+        <thead className="text-xs text-gray-700 uppercase border-b border-gray-200">
+          <tr>
+            {columns.map((col, index) => (
+              <th
+                className={`p-5 tracking-wider font-medium text-[15px] whitespace-nowrap`}
+                key={`head-${index}`}
+                scope="col"
+              >
+                <div className="flex items-center gap-2">{col.header}</div>
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        {/* Body */}
+        <tbody className="divide-y divide-gray-100">{renderBodyContent()}</tbody>
+      </table>
+    </div>
+  );
+};
