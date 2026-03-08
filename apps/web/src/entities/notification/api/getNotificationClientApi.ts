48e1d7d3e8c6d459bfb2a9bcd678f389bf050517
박승연 <70251709+40food@users.noreply.github.com>
2026-03-08 23:00:40 +0900

    [HOTSPOT-284] feat: 알림 페이지 무한 스크롤 구현

--- apps/web/src/entities/notification/api/getNotificationClientApi.ts
+++ apps/web/src/entities/notification/api/getNotificationClientApi.ts
@@ -1,8 +1,13 @@
 import { api } from '@/shared/api/client';
 import type { ApiResponse } from '@/shared/api/types';
-import type { GetNotificationResponse } from './types';
+import type { GetNofiticationRequest, GetNotificationResponse } from './types';
 
-export const getNotificationClientApi = async () => {
-  const { data } = await api.get<ApiResponse<GetNotificationResponse>>(`/api/v1/notifications`);
+export const getNotificationClientApi = async ({ page, size }: GetNofiticationRequest) => {
+  const { data } = await api.get<ApiResponse<GetNotificationResponse>>(`/api/v1/notifications`, {
+    params: {
+      page,
+      size,
+    },
+  });
   return data.data;
 };
