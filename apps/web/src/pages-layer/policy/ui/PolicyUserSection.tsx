b2056c5424786f40028c7fa9223800fb41cda1e7
40food <40food@naver.com>
2026-03-09 11:29:01 +0900

    [HOTSPOT-283] fix: 가족 정책 리스트 구분선 잘림 해결

--- apps/web/src/pages-layer/policy/ui/PolicyUserSection.tsx
+++ apps/web/src/pages-layer/policy/ui/PolicyUserSection.tsx
@@ -9,7 +9,7 @@ export const PolicyUserSection = ({ data }: PolicyUserSectionProps) => {
     <div>
       {data.memberPolicies.map((i, index) => (
         <div key={i.memberId}>
-          {index !== 0 && <div className="w-full h-[0.5px] bg-gray-200" />}
+          {index !== 0 && <div className="w-full h-px bg-gray-200" />}
           <PolicyUserCard data={i} familyId={data.familyId} />
         </div>
       ))}
