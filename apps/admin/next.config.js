2accc8bc17640b96f3810d0639b89706a39788ef
Andrew <98813748+starboxxxx@users.noreply.github.com>
2026-03-09 21:59:30 +0900

    ADMIN CD CREATE (#118)
    * feat: 프론트 CD 배포 로직 구현
    
    * chore: remove ecs temp task definition files
    
    * fix: modify jq
    
    * feat: front admin cd 추가
    
    * feat: front admin cd 추가
    
    * feat: front admin cd 추가
    
    * feat: front admin cd 추가
    
    * Fix formatting in ECS service update command
    
    * Update Dockerfile for admin with user permissions

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+
   transpilePackages: ['@hotspot/ui'],
+
   turbopack: {
     rules: {
       '*.svg': {
@@ -11,5 +14,4 @@ const nextConfig = {
   },
 };
 
-// biome-ignore lint/style/noDefaultExport: <기본 세팅>
 export default nextConfig;
