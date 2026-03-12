514ab3816c7175edacac1459155f6c489d1b7d4d
40food <40food@naver.com>
2026-03-12 19:18:19 +0900

    [HOTSPOT-324] fix: 선물 데이터 세부 조절 버튼 삭제

--- apps/web/src/features/gift/present-data/ui/PresentAmount.tsx
+++ apps/web/src/features/gift/present-data/ui/PresentAmount.tsx
@@ -12,27 +12,6 @@ export const PresentAmount = ({ presentAmount, handleSelect }: PresentAmountProp
     <div className="gap-1 flex flex-col">
       <p className="text-[13px] font-bold">선물할 데이터 양</p>
       <p className="text-[11px] text-gray-500">선물할 데이터 양을 선택하세요.</p>
-      <div className="flex flex-row gap-4 items-center justify-center pt-3">
-        <button
-          className="p-3 border bg-gray-50 border-gray-200 rounded-xl hover:bg-gray-100"
-          onClick={() => {
-            handleSelect(presentAmount - 1);
-          }}
-          type="button"
-        >
-          <MinusIcon className="w-4 h-4 text-gray-500" />
-        </button>
-        <p className="text-[23px] font-bold">{presentAmount.toFixed(1)}GB</p>
-        <button
-          className="p-3 border bg-gray-50 border-gray-200 rounded-xl hover:bg-gray-100"
-          onClick={() => {
-            handleSelect(presentAmount + 1);
-          }}
-          type="button"
-        >
-          <PlusIcon className="w-4 h-4 text-gray-500" />
-        </button>
-      </div>
       <GiftAmountButtons handleSelect={handleSelect} presentAmount={presentAmount} />
     </div>
   );
