d0516d6fa1dc4b553e8f626fd03bd3ea8226a9a8
박승연 <70251709+40food@users.noreply.github.com>
2026-03-22 21:46:36 +0900

    [HOTSPOT-373] fix: block label 디자인 수정

--- apps/web/src/domains/user/ui/UserBlockLabel.tsx
+++ apps/web/src/domains/user/ui/UserBlockLabel.tsx
@@ -5,7 +5,7 @@ interface UserBlockLabelProps {
 export const UserBlockLabel = ({ isBlocked }: UserBlockLabelProps) => {
   if (!isBlocked) return null;
   return (
-    <span className="text-xs py-1 px-2 border-2 border-red-100 bg-red-100 text-red-700 rounded-sm">
+    <span className="text-[0.625rem] font-medium leading-none p-4 border-2 border-red-100 bg-red-100 text-red-700 rounded-lg">
       차단
     </span>
   );
