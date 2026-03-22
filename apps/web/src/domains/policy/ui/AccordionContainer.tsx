cf962447c1d5a4787365950cbbdf2f94f93594ec
박승연 <70251709+40food@users.noreply.github.com>
2026-03-22 21:49:06 +0900

    [HOTSPOT-373] fix: 가족 정책 리스트 아코디언과 구분선 사이 여백 추가

--- apps/web/src/domains/policy/ui/AccordionContainer.tsx
+++ apps/web/src/domains/policy/ui/AccordionContainer.tsx
@@ -14,7 +14,7 @@ export const AccordionContainer = ({ policyList, blockServices }: AccordionConta
   console.log(policyList);
 
   return (
-    <div className="flex gap-1.5 flex-col">
+    <div className="flex gap-1.5 flex-col pb-8">
       <Accordion
         title={
           <div className="flex items-center gap-8 p-16 font-heading-heading4">
